ed11b7246202f45936b724873ad2c22606fabb05
diva_venkat <168173895+divaashree@users.noreply.github.com>
2025-03-03 13:15:26 +0800

    Create Ascii Value.java

---
+++ Ascii Value.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main
+{
+public static void main(String[] args) {
+char ch=&#39;A&#39;;
+int castAscii=(int) ch;
+System.out.println(&quot;The Ascii value of&quot;+ch+&quot;is:&quot;+castAscii);
+}
+}
